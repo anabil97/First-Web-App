39362d1ca401827ebd227c538a6876913f1f1114
Ahmed Nabil <ahmednabil97.an.com>
2022-09-21 00:01:48 +0200

    new

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 import requests
 
-
 my_app = Flask(__name__)
 
 
@@ -18,61 +17,40 @@ def homepage():
 def getdata():
     return render_template("getdata.html", pagetitle="DataPage")
 
-# def app(url):
 
-#     engine = create_engine("sqlite:///web.db")
+#########################################################################
+def app(url):
 
-#     meta_data = MetaData()
+    engine = create_engine("sqlite:///web.db")
 
-#     products = Table(
-#         "products", meta_data,
-#         Column("id", Integer, primary_key=True),
-#         Column("title", String),
-#         Column("category", String),
-#         Column("rating", String),
-#         Column("price", String)
-#     )
-#     meta_data.create_all(engine)
+    meta_data = MetaData()
 
-#     response = requests.get(url)
-#     file = response.json()
+    products = Table(
+        "products", meta_data,
+        Column("id", Integer, primary_key=True),
+        Column("title", String),
+        Column("category", String),
+        Column("rating", String),
+        Column("price", Integer)
+    )
+    meta_data.create_all(engine)
 
-#     file1 = file["products"]
+    response = requests.get(url)
+    file = response.json()
 
-#     for i in file1:
-#         x1 = i["title"]
-#         x2 = i["category"]
-#         x3 = i["rating"]
-#         x4 = i["price"]
-#         ins = products.insert().values(title=x1, category=x2, rating=x3, price=x4)
-#         conn = engine.connect()
-#         result = conn.execute(ins)
-#     return 
+    file1 = file["products"]
 
-# def getdata(x, y):
-#     engine = create_engine("sqlite:///web.db")
-#     meta_data = MetaData()
+    for i in file1:
+        x1 = i["title"]
+        x2 = i["category"]
+        x3 = i["rating"]
+        x4 = i["price"]
+        ins = products.insert().values(title=x1, category=x2, rating=x3, price=x4)
+        conn = engine.connect()
+        result = conn.execute(ins)
+    return
 
-#     products = Table(
-#         "products", meta_data,
-#         Column("id", Integer, primary_key=True),
-#         Column("title", String),
-#         Column("category", String),
-#         Column("rating", String),
-#         Column("price", String)
-#     )
-
-#     prod = products.select()
-#     conn = engine.connect()
-#     result = conn.execute(prod)
-#     data = []
-#     for i in result:
-#         if int(i[4]) >= x and int(i[4]) <= y:
-#             data.append(i)
-#     return data
-
-
-
+#############################################################################
 
 if __name__ == "__main__":
     my_app.run(debug=True, port=8000)
---
+++ myweb.py
@@ -0,0 +1,73 @@
+from dbm import dumb
+from itertools import product
+from tokenize import Ignore
+from unittest import result
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+import requests
+import json
+import yaml
+from sqlalchemy.dialects.postgresql import insert
+
+url = input("please enter url")
+
+
+def app(url):
+
+    engine = create_engine("sqlite:///web.db")
+
+    meta_data = MetaData()
+
+    products = Table(
+        "products", meta_data,
+        Column("id", Integer, primary_key=True),
+        Column("title", String),
+        Column("category", String),
+        Column("rating", String),
+        Column("price", Integer)
+    )
+    meta_data.create_all(engine)
+
+    response = requests.get(url)
+    file = response.json()
+
+    file1 = file["products"]
+
+    for i in file1:
+        x1 = i["title"]
+        x2 = i["category"]
+        x3 = i["rating"]
+        x4 = i["price"]
+        ins = products.insert().values(title=x1, category=x2, rating=x3, price=x4)
+        conn = engine.connect()
+        result = conn.execute(ins)
+    return
+
+app(url)
+
+
+x = int(input("please enter price range from"))
+y = int(input("please enter price range to"))
+
+def getdata(x, y):
+    engine = create_engine("sqlite:///web.db")
+    meta_data = MetaData()
+
+    products = Table(
+        "products", meta_data,
+        Column("id", Integer, primary_key=True),
+        Column("title", String),
+        Column("category", String),
+        Column("rating", String),
+        Column("price", Integer)
+    )
+
+    prod = products.select()
+    conn = engine.connect()
+    result = conn.execute(prod)
+    data = []
+    for i in result:
+        if int(i[4]) >= x and int(i[4]) <= y:
+            data.append(i)
+    return data
+
+print(getdata(int(x),int(y)))
